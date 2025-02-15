0e0937a094383e1a6e84648e074fb49044d583f2
20100 <mmmelkady23@gmail.com>
2025-02-16 00:08:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static LinqOne.ListGenerator;
+namespace LinqOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region One :1. Find all products that are out of stock
+            var OutOfStock = ProductList.Where(p => p.UnitsInStock == 0);
+
+            foreach (var item in OutOfStock)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------Another Solve---------");
+
+            var OutOfStock02 =from p in ProductList
+                              where p.UnitsInStock == 0
+                              select p;
+            foreach (var item in OutOfStock02)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+        }
+    }
+}
